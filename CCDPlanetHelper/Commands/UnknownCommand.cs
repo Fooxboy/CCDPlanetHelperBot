f0865d2baae705788d0546ddbd7db51c760104b0
Fooxboy <Fooxboy@yandex.ru>
2021-02-03 18:54:40 +0300

    hello world!

---
+++ CCDPlanetHelper/Commands/UnknownCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fooxboy.NucleusBot.Interfaces;
+using Fooxboy.NucleusBot.Models;
+
+namespace CCDPlanetHelper.Commands
+{
+    public class UnknownCommand :INucleusCommand
+    {
+        public string Command => "inknown";
+        public string[] Aliases => new string[0];
+        public void Execute(Message msg, IMessageSenderService sender, IBot bot)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Init(IBot bot, ILoggerService logger)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ CCDPlanetHelper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Fooxboy.NucleusBot;
+using Fooxboy.NucleusBot.Enums;
+using Fooxboy.NucleusBot.Interfaces;
+using Fooxboy.NucleusBot.Models;
+using Fooxboy.NucleusBot.Services;
+
+namespace CCDPlanetHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botSettings = new BotSettings();
+            botSettings.GroupId = 0;
+            botSettings.Messenger = MessengerPlatform.Vkontakte;
+            botSettings.VKToken = "null";
+
+            var command = new Commands.UnknownCommand();
+
+            var bot = new Bot(botSettings, command);
+        }
+    }
+}
